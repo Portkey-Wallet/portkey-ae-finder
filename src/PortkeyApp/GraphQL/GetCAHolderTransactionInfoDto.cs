1a14834fcfd64a1eb9f188a8787b37982e5a60f4
felix-zhaolei <felix@portkey.finance>
2024-12-25 14:38:57 +0800

    modify: GetCAHolderTransactionInfoDto.

--- src/PortkeyApp/GraphQL/GetCAHolderTransactionInfoDto.cs
+++ src/PortkeyApp/GraphQL/GetCAHolderTransactionInfoDto.cs
@@ -4,21 +4,21 @@ namespace PortkeyApp.GraphQL;
 
 public class GetCAHolderTransactionInfoDto : PagedResultRequestDto
 {
-    public string ChainId { get; set; }
+    public string? ChainId { get; set; }
 
-    public long StartBlockHeight { get; set; }
+    public long? StartBlockHeight { get; set; }
 
-    public long EndBlockHeight { get; set; }
+    public long? EndBlockHeight { get; set; }
 
-    public string Symbol { get; set; }
+    public string? Symbol { get; set; }
 
     [Name("caAddresses")] public List<string?>? CAAddresses { get; set; }
 
-    public string TransactionId { get; set; }
+    public string? TransactionId { get; set; }
 
-    public string BlockHash { get; set; }
+    public string? BlockHash { get; set; }
 
-    public string TransferTransactionId { get; set; }
+    public string? TransferTransactionId { get; set; }
 
-    public List<string> MethodNames { get; set; } = new();
+    public List<string?>? MethodNames { get; set; } = new();
 }
